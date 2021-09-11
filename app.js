0a7a0ec52ac794652160726d89e7e3eb6c2b1587
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-09-11 11:35:20 -0500

    feat: create auth middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
+const auth = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/users');
 const errorHandler = require('./middlewares/errorHandler');
 
@@ -25,6 +26,7 @@ app.use(express.urlencoded({
 app.post('/signin', login);
 app.post('/signup', createUser);
 
+app.use(auth);
 app.use('/users', require('./routes/users'));
 
 app.use(errorHandler);
---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const { UnauthorizedError } = require('../errors/index');
+const { JWT_SECRET } = require('../utils/constants');
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  if (!req.cookies.jwt) {
+    throw new UnauthorizedError();
+  }
+
+  const token = req.cookies.jwt;
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    throw new UnauthorizedError();
+  }
+
+  req.user = payload;
+
+  next();
+};
